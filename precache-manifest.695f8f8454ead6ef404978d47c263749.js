f7c6492394047527fb45fd1318f446de672c704f
Коляченко Максим Александрович <Kolyachenko.M.A@sberbank.ru>
2020-10-16 16:53:08 +0300

    Updates

---
+++ precache-manifest.695f8f8454ead6ef404978d47c263749.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999fa922411dc3e867191e5739bce45d",
+    "url": "/artyom-birthday-quest/index.html"
+  },
+  {
+    "revision": "1baf47d2415bb07d7d9f",
+    "url": "/artyom-birthday-quest/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8e7323c10991775806f8",
+    "url": "/artyom-birthday-quest/static/js/2.31594020.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/artyom-birthday-quest/static/js/2.31594020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1baf47d2415bb07d7d9f",
+    "url": "/artyom-birthday-quest/static/js/main.dd67cbe9.chunk.js"
+  },
+  {
+    "revision": "2e2b9b18f920d090e9c1",
+    "url": "/artyom-birthday-quest/static/js/runtime-main.23888786.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/artyom-birthday-quest/precache-manifest.695f8f8454ead6ef404978d47c263749.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/artyom-birthday-quest/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
